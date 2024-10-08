9c66eb62491e9ae305db98b5f01298cb1dc095a2
gizmonster <jbaynard4@nc.rr.com>
2024-10-08 17:42:54 -0400

    Updated filter's item hitbox to be a full block outside of the filter chute
    Signed-off-by: gizmonster <jbaynard4@nc.rr.com>

--- src/main/java/monster/giz/blocks/FilterBlock.java
+++ src/main/java/monster/giz/blocks/FilterBlock.java
@@ -42,13 +42,21 @@ public class FilterBlock extends BlockWithEntity {
             );
 
     // Represents the hitbox as items see it, allowing for items to pass through the "chute"
-    public static final VoxelShape ITEM_HITBOX = VoxelShapes.union(
+    public static final VoxelShape ITEM_HITBOX_old = VoxelShapes.union(
             VoxelShapes.cuboid(0, 0.625, 0, 1, 1, 1),
             VoxelShapes.cuboid(0.1875, 0, 0.125, 0.8125, 0.625, 0.1875),
             VoxelShapes.cuboid(0.1875, 0, 0.8125, 0.8125, 0.625, 0.875),
             VoxelShapes.cuboid(0.8125, 0, 0.125, 0.875, 0.625, 0.875),
             VoxelShapes.cuboid(0.125, 0, 0.125, 0.1875, 0.625, 0.875)
     );
+
+    public static final VoxelShape ITEM_HITBOX = VoxelShapes.union(
+            VoxelShapes.cuboid(0, 0.625, 0, 1, 1, 1),
+            VoxelShapes.cuboid(0.25, 0, 0, 0.75, 0.625, 0.25),
+            VoxelShapes.cuboid(0.25, 0, 0.75, 0.75, 0.625, 1),
+            VoxelShapes.cuboid(0.75, 0, 0, 1, 0.625, 1),
+            VoxelShapes.cuboid(0, 0, 0, 0.25, 0.625, 1)
+            );
 
     // Collision for drawing the outline
     public static final VoxelShape OUTLINE_SHAPE = VoxelShapes.union(
