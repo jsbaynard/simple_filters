fae85ea49a13839ae9a060bf4cb5154951de305b
gizmonster <jbaynard4@nc.rr.com>
2024-10-12 19:32:34 -0400

    Items that use the block item model now appear larger in the frame for improved visual clarity.
    Signed-off-by: gizmonster <jbaynard4@nc.rr.com>

--- src/client/java/monster/giz/simple_filters/render/block/entity/FilterBlockEntityRenderer.java
+++ src/client/java/monster/giz/simple_filters/render/block/entity/FilterBlockEntityRenderer.java
@@ -2,21 +2,27 @@ package monster.giz.simple_filters.render.block.entity;
 
 import monster.giz.simple_filters.blocks.FilterBlock;
 import monster.giz.simple_filters.blocks.entity.FilterBlockEntity;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
-import net.minecraft.world.World;
 
 public class FilterBlockEntityRenderer implements BlockEntityRenderer<FilterBlockEntity> {
 
+    private final ItemRenderer itemRenderer;
+    public static final float VANILLA_BLOCK_ITEM_SCALE_FACTOR = 0.375f;
+
     public FilterBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
         super();
+        this.itemRenderer = context.getItemRenderer();
     }
 
     // Yes, this is disgusting. No, I'm not messing with this anymore.
@@ -29,53 +35,57 @@ public class FilterBlockEntityRenderer implements BlockEntityRenderer<FilterBloc
 
         Item item = entity.getFilteredItem();
         Direction facing = entity.getCachedState().get(FilterBlock.FACING);
+        BakedModel model = itemRenderer.getModel(item.getDefaultStack(), null, null, 0);
 
         float scale = 0.35F;
 
+        // This is a design choice, wanted items that have the block item model to appear larger in the frame
+        // for better visual clarity.
+        if (item instanceof BlockItem) {
+            if (hasBlockItemTransformations(model)) {
+                scale = 0.5F;
+            }
+        }
+
         switch (facing) {
             case WEST:
-                matrices.translate(0.175, 0.30, 0.5);
+                matrices.translate(0.175, 0.31, 0.5);
                 matrices.scale(scale, scale, scale);
                 matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(90));
                 break;
             case EAST:
-                matrices.translate(0.825, 0.30, 0.5);
+                matrices.translate(0.825, 0.31, 0.5);
                 matrices.scale(scale, scale, scale);
                 matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(270));
                 break;
             case NORTH:
-                matrices.translate(0.5, 0.30, 0.175);
+                matrices.translate(0.5, 0.31, 0.175);
                 matrices.scale(scale, scale, scale);
                 matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180));
                 matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180));
                 matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
                 break;
             case SOUTH:
-                matrices.translate(0.5, 0.30, 0.825);
+                matrices.translate(0.5, 0.31, 0.825);
                 matrices.scale(scale, scale, scale);
                 matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(360));
                 matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(180));
                 matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180));
                 break;
         }
-        renderItem(item, entity.getWorld(), matrices, vertexConsumers, light, overlay);
+        renderItem(model, item.getDefaultStack(), matrices, vertexConsumers, light, overlay);
     }
 
-    private void renderItem(Item item, World world, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    private boolean hasBlockItemTransformations(BakedModel model) {
+        if (model.getTransformation().thirdPersonRightHand.scale.x == VANILLA_BLOCK_ITEM_SCALE_FACTOR) {
+            return true;
+        }
+        return false;
+    }
 
+    private void renderItem(BakedModel model, ItemStack stack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push();
-
-        MinecraftClient.getInstance().getItemRenderer().renderItem(
-                item.getDefaultStack(),
-                ModelTransformationMode.FIXED,
-                light,
-                overlay,
-                matrices,
-                vertexConsumers,
-                world,
-                0
-        );
-
+        itemRenderer.renderItem(stack, ModelTransformationMode.FIXED, false, matrices, vertexConsumers, light, overlay, model);
         matrices.pop();
 
     }
